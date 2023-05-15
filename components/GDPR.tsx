112bdd3984faa8cef395451d2fb928ef64553ce4
Sebastiaan Colmen <skolmschate@hotmail.nl>
2023-05-15 13:17:33 -0500

    added basic GDPR functionality

---
+++ components/GDPR.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { useState, useEffect} from "react";
+import Cookies from "js-cookie";
+
+
+export default function GDPR( {fullScreen}: boolean ) {
+    const [showGDPRWindow, setShowGDPRWindow] = useState(false);
+    const [gdpr, setGDPR] = useState("")
+
+    const fullScreenClassName = "fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-50`"
+    const fullScreenClassName2 = "absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-blur-sm"
+    const normalClassName = "m-3"
+
+    const activeButton = "text-md font-semibold text-white bg-black p-1 m-2"
+    const inactiveButton = "text-md font-semibold text-black p-1 m-2"
+
+    const handleAccept = () => {
+        Cookies.set('gdpr', 'accepted');
+        setGDPR("accepted");
+        if (fullScreen == false) {
+            setShowGDPRWindow(true);
+        }
+        else
+        {
+            setShowGDPRWindow(false);
+        }
+    }
+
+    const handleBasic = () => {
+        Cookies.set('gdpr', 'basic');
+        setGDPR("basic");
+        if (fullScreen == false) {
+            setShowGDPRWindow(true);
+        }
+        else
+        {
+            setShowGDPRWindow(false);
+        }
+    }
+
+    useEffect(() => {
+        const cookie: string = String(Cookies.get('gdpr'));
+        setGDPR(cookie);
+        if (fullScreen == false) {
+            setShowGDPRWindow(true);
+        }
+        if (cookie === undefined) {
+            setShowGDPRWindow(true);
+        }
+    }, []);
+
+    if (!showGDPRWindow) {
+        return null;
+    }
+
+  return (
+    <div className={`${fullScreen ? fullScreenClassName : normalClassName}`}>
+        <div className={`${fullScreen ? fullScreenClassName2 : normalClassName}`}>
+        </div>
+        <div className="relative bg-white text-black text-center p-3">
+            <h1 className="text-2xl font-bold">GDPR</h1>
+            <p>All the GDPR stuff goes here</p>
+
+            {/* GDPR stuff */}
+
+            <div>
+                <button className={gdpr == "accepted" ? activeButton : inactiveButton} onClick={handleAccept}>Accept</button>
+            </div>
+            <div>
+                <button className={gdpr == "basic" ? activeButton : inactiveButton} onClick={handleBasic}>Basic</button>
+            </div>
+        </div>
+  </div>
+  );
+  }
