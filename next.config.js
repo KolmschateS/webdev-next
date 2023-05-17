18f0b7de6cde08b2afcde397a86bf5b2b4b7ce2a
Sebastiaan Colmen <skolmschate@hotmail.nl>
2023-05-16 22:32:16 -0500

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import('next').NextConfig} */ 
+const nextConfig = {
+
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=31536000; includeSubDomains; preload;"
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY"
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "frame-ancestors 'none';"
+                    }
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
