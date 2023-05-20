dd1c84849554f7c7bc87cb376375b46339fa312a
Sebastiaan Colmen <skolmschate@hotmail.nl>
2023-05-20 12:01:04 -0500

    updated cookies safety

--- app/about/actions.ts
+++ app/about/actions.ts
@@ -10,13 +10,24 @@ export async function updateCookie()
     // set visits and return value
     if (visitsCookie == undefined)
     {
+      // set cookie with visits or 0, set with httpOnly and secure
         // @ts-ignore
-        cookies().set('visits', 0);
+        cookies().set('visits', 0,
+        {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict'
+        });
         return 0;
     }
     else{
         // @ts-ignore
-        cookies().set('visits', Number(visitsCookie.value) + 1);
+        cookies().set('visits', Number(visitsCookie.value) + 1, 
+        {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict'
+        });
         return Number(visitsCookie.value) + 1;
     }
   }
